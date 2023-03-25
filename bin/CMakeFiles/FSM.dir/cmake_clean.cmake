d743ff671851de67d9144717a4bf09c5c9fab6cc
Amin <amin.aghakhani71@gmail.com>
2023-03-25 14:25:45 -0700

    danish is removed from this project.

---
+++ bin/CMakeFiles/FSM.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FSM.dir/examples/ex_fsm.o"
+  "CMakeFiles/FSM.dir/src/delay.o"
+  "CMakeFiles/FSM.dir/src/fsm.o"
+  "FSM"
+  "FSM.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FSM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
